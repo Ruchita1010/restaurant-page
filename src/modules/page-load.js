980b23ac43abe224281fb6154108d75250afd472
Ruchita Gosavi <ruchigosavi10@gmail.com>
2022-09-03 23:50:57 +0530

    Refactor code and make hamburger menu functional

--- src/modules/page-load.js
+++ src/modules/page-load.js
@@ -10,19 +10,33 @@ const createNavbar = () => {
         </div>
         <ul class="nav-links">
             <li class="list-item"><a href="#">Home</a></li>
-            <li class="list-item"><a href="#">Contact</a></li>
+            <li class="list-item"><a href="#">Menu</a></li>
             <li class="list-item"><a href="#">About</a></li>
         </ul>
     `;
+    const hamburger = navElem.firstElementChild;
+    const navLinks = hamburger.nextElementSibling;
+    const listItem = navLinks.children;
+    hamburger.addEventListener("click", () => {
+        navLinks.classList.toggle("click");
+        Array.from(listItem).forEach(item => {
+            item.classList.toggle("fade");
+        });
+    });
     return navElem;
 }
 
-const pageLoad = () => {
-    const content = document.querySelector(".content");
+const createBgContainer = () => {
     const bgContainer = document.createElement("div");
     bgContainer.classList.add("bg-container");
     const navbar = createNavbar();
     bgContainer.appendChild(navbar);
+    return bgContainer;
+}
+
+const pageLoad = () => {
+    const content = document.querySelector(".content");
+    const bgContainer = createBgContainer();
     content.appendChild(bgContainer);
 }
 
