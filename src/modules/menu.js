3c9c441ad96f2a99c6526e529fdc65e8b57297bc
Ruchita Gosavi <ruchigosavi10@gmail.com>
2022-09-05 00:22:06 +0530

    Add menu and padding to main element

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,5 +1,69 @@
+import "../styles/menu.css";
+// Import all menu food assets
+import pizzas from "../assets/pizzas.png";
+import pastas from "../assets/pastas.png";
+import ramen from "../assets/ramen.png";
+import sushi from "../assets/sushi.png";
+import desserts from "../assets/desserts.png";
+import drinks from "../assets/drinks.png";
+
+const menuItems = [
+    {
+        name: "Pizzas",
+        imageUrl: pizzas,
+    },
+    {
+        name: "Pastas",
+        imageUrl: pastas,
+    },
+    {
+        name: "Ramen",
+        imageUrl: ramen,
+    },
+    {
+        name: "Sushi",
+        imageUrl: sushi,
+    },
+    {
+        name: "Desserts",
+        imageUrl: desserts,
+    },
+    {
+        name: "Drinks",
+        imageUrl: drinks,
+    },
+];
+
+const createMenuItem = (name, imageUrl) => {
+    const menuItem = document.createElement("div");
+    menuItem.classList.add("menu-item");
+
+    const itemName = document.createElement("p");
+    itemName.classList.add("item-name");
+    itemName.innerText = name;
+
+    const imgContainer = document.createElement("div");
+    imgContainer.classList.add("img-container");
+    const imgElem = new Image();
+    imgElem.classList.add("item-img");
+    imgElem.src = imageUrl;
+    imgContainer.appendChild(imgElem);
+
+    menuItem.appendChild(itemName);
+    menuItem.appendChild(imgElem);
+    return menuItem;
+}
+
 const loadMenu = () => {
-    console.log("Menu Loaded");
+    const main = document.querySelector("main");
+    const menu = document.createElement("div");
+    menu.classList.add("menu");
+    menuItems.forEach(menuItem => {
+        const menuItemElem = createMenuItem(menuItem.name, menuItem.imageUrl);
+        menu.appendChild(menuItemElem);
+    });
+    main.textContent = "";
+    main.appendChild(menu);
 }
 
 export default loadMenu;
