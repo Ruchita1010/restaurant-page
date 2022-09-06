9d5918fb7f00ff81aba3dddb6ea5442a7e2e4eba
Ruchita Gosavi <ruchigosavi10@gmail.com>
2022-09-06 16:59:02 +0530

    Add intro to the home page

--- src/modules/home.js
+++ src/modules/home.js
@@ -1,11 +1,23 @@
+import "../styles/home.css";
+
 const loadHome = () => {
     const main = document.querySelector("main");
-    const container = document.createElement("div");
-    const introText = document.createElement("p");
-    introText.innerText = "Welcome djkfdhgdfkjg skdkfhjdsfh dfjkg";
-    container.appendChild(introText);
+    const home = document.createElement("div");
+    home.classList.add("home");
+
+    const intro = document.createElement("div");
+    intro.classList.add("intro");
+
+    const greeting = document.createElement("h2");
+    greeting.innerText = "Welcome To Hunimal";
+    const tagline = document.createElement("p");
+    tagline.innerText = `A magical place where animals and humans eat together`;
+
+    intro.appendChild(greeting);
+    intro.appendChild(tagline);
+    home.appendChild(intro);
     main.textContent = "";
-    main.appendChild(container);
+    main.appendChild(home);
 }
 
 export default loadHome;
