5a32e435d37db7f05430b14082fcb045dd81d63b
Ruchita Gosavi <ruchigosavi10@gmail.com>
2022-09-06 19:44:58 +0530

    Add relevant text in about page

--- src/modules/about.js
+++ src/modules/about.js
@@ -7,7 +7,17 @@ const loadAbout = () => {
     const about = document.createElement("div");
     about.classList.add("about");
     const text = document.createElement("p");
-    text.innerHTML = `dhfkhd dkjfhdkfhd gfgjhdfg kdfghfdjg`;
+    text.innerHTML = `
+        <p>Hunimal is a restaurant in the mysterious and magical land of Aurorealis.</p>
+        <p>It is the only restaurant where you will find humans and animals (residents of Aurorealis) eating together.</p>
+        <p>Hunimal has a variety of food items that humans do eat in their world but it has the most unique taste.</p>
+        <p>"No restaurant in the human world can even come close to what Hunimal offers" </p>
+        <p> - is what a human once said (¬‿¬)</p>
+        <p>Well, you won't know until you try, right?</p>
+        <p>Better luck getting here because (￣▽￣)</p>
+        <p>Only humans with a pure heart can enter the land of Aurorealis</p>
+        <p class="hearts">❤ ❤ ❤</p>
+    `;
     about.appendChild(text);
     wrapper.appendChild(about);
     main.textContent = "";
