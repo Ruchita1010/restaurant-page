8b3e07a7dad1f8b891757b184044d5095ca25265
Ruchita Gosavi <ruchigosavi10@gmail.com>
2022-09-07 23:42:50 +0530

    Set font family

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,10 @@ module.exports = {
             {
                 test: /\.(png|jpg|jpeg|svg|gif)$/i,
                 type: "asset/resource",
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
             }
         ],
     },
